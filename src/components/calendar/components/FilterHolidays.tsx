60e60294be476d55d836988542841a7e33717e1f
carlosjgb <carlosborrego@capitole-consulting.com>
2023-09-04 13:07:48 +0200

    filters trad

--- src/components/calendar/components/FilterHolidays.tsx
+++ src/components/calendar/components/FilterHolidays.tsx
@@ -8,7 +8,6 @@ import { IGroup } from "@/components/interfaces/group";
 import { IUser } from "@/components/interfaces/user";
 import { Controller, useForm } from "react-hook-form";
 import { useState } from "react";
-import { group } from "console";
 
 interface Props {
     holidays: IHoliday[];
@@ -36,8 +35,9 @@ export default function FilterHolidays({ holidays, groups, dict }: Props) {
     const [filteredGroup, setFilteredGroup] = useState(groups);
     const [filteredHolidays, setFilteredHolidays] = useState(holidays);
     const [filteredUser, setFilteredUser] = useState(listUsersFilter);
+    const [clearAll, setClearAll] = useState(false);
 
-    const { register, handleSubmit, control } = useForm<Input>({
+    const { handleSubmit, control } = useForm<Input>({
         defaultValues: { groups: groups, users: listUsersFilter },
     });
 
@@ -58,6 +58,7 @@ export default function FilterHolidays({ holidays, groups, dict }: Props) {
     };
 
     const handleCheckboxGroup = (group: any) => {
+        setClearAll(false);
         if (filteredGroup.find((item) => item?.id === group?.id)) {
             setFilteredGroup(filteredGroup.filter((item: any) => item?.id !== group?.id));
         } else {
@@ -66,6 +67,7 @@ export default function FilterHolidays({ holidays, groups, dict }: Props) {
     };
 
     const handleCheckboxUser = (user: any) => {
+        setClearAll(false);
         if (filteredUser.find((item) => item?.id === user?.id)) {
             setFilteredUser(filteredUser.filter((item: any) => item.id !== user?.id));
         } else {
@@ -73,16 +75,31 @@ export default function FilterHolidays({ holidays, groups, dict }: Props) {
         }
     };
 
+    const handleClearAll = () => {
+        setFilteredGroup([]);
+        setFilteredUser([]);
+        setClearAll(true);
+    };
+
+    const handleSelectAllGroups = () => {
+        setClearAll(false);
+        setFilteredGroup(groups);
+    };
+
+    const handleSelectAllUsers = () => {
+        setClearAll(false);
+        setFilteredUser(listUsersFilter);
+    };
+
     return (
         <form onSubmit={handleSubmit(onSubmit)} action="#" method="POST">
             <div className="bg-white">
-                {/* Filters */}
                 <Disclosure
                     as="section"
                     aria-labelledby="filter-heading"
                     className="grid items-center border-b  border-gray-200">
                     <h2 id="filter-heading" className="sr-only">
-                        Filters
+                        {dict?.calendar?.filter?.filters}
                     </h2>
                     <div className="relative py-4 flex justify-between w-full">
                         <div className="flex space-x-6 divide-x divide-gray-200 px-4 text-sm sm:px-6 lg:px-8">
@@ -92,25 +109,38 @@ export default function FilterHolidays({ holidays, groups, dict }: Props) {
                                         className="mr-2 h-5 w-5 flex-none text-gray-400 group-hover:text-gray-500"
                                         aria-hidden="true"
                                     />
-                                    2 Filters
+                                    2 {dict?.calendar?.filter?.filters}
                                 </Disclosure.Button>
                             </div>
                             <div className="pl-6">
-                                <button type="button" className="text-gray-500">
-                                    Clear all
+                                <button
+                                    type="button"
+                                    onClick={handleClearAll}
+                                    className="text-gray-400">
+                                    {dict?.calendar?.filter?.clearAll}
                                 </button>
                             </div>
                         </div>
                         <button
                             type="submit"
                             className="rounded-full bg-primary px-2.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">
-                            Filtrar
+                            {dict?.calendar?.filter?.button}
                         </button>
                     </div>
                     <Disclosure.Panel className="border-t border-gray-200 py-10">
                         <div className="mx-auto grid grid-cols-1 gap-x-4 px-4 text-sm sm:px-6 md:gap-x-6 lg:px-8">
                             <fieldset>
-                                <legend className="block font-medium mb-2.5">Groups</legend>
+                                <div className="flex space-x-6 divide-x items-center border-gray-200">
+                                    <legend className="block font-medium mb-2.5 text-black capitalize">
+                                        {dict?.calendar?.filter?.groups}
+                                    </legend>
+                                    <button
+                                        type="button"
+                                        onClick={handleSelectAllGroups}
+                                        className="block mb-2.5 pl-6 text-gray-400">
+                                        {dict?.calendar?.filter?.selectAll}
+                                    </button>
+                                </div>
                                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-y-2 gap-x-20">
                                     {groups?.map((option) => (
                                         <Controller
@@ -125,9 +155,10 @@ export default function FilterHolidays({ holidays, groups, dict }: Props) {
                                                         id={`group-${option?.id}`}
                                                         name={`group-${option?.id}`}
                                                         value={option?.name}
-                                                        defaultChecked={filteredGroup.includes(
-                                                            option
-                                                        )}
+                                                        checked={
+                                                            filteredGroup.includes(option) &&
+                                                            !clearAll
+                                                        }
                                                         type="checkbox"
                                                         className="h-4 w-4 flex-shrink-0 rounded border-gray-300 text-primary focus:ring-primary"
                                                     />
@@ -149,8 +180,18 @@ export default function FilterHolidays({ holidays, groups, dict }: Props) {
                             </fieldset>
 
                             <fieldset className="mt-10">
-                                <legend className="block font-medium mb-2.5">Users</legend>
-                                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-y-2 gap-x-20">
+                                <div className="flex space-x-6 divide-x items-center border-gray-200">
+                                    <legend className="block font-medium mb-2.5 text-black capitalize">
+                                        {dict?.calendar?.filter?.users}
+                                    </legend>
+                                    <button
+                                        type="button"
+                                        onClick={handleSelectAllUsers}
+                                        className="block mb-2.5 pl-6 text-gray-400">
+                                        {dict?.calendar?.filter?.selectAll}
+                                    </button>
+                                </div>
+                                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-y-2 gap-x-20 ">
                                     {listUsersFilter?.map((option) => (
                                         <Controller
                                             key={option.id}
@@ -164,9 +205,10 @@ export default function FilterHolidays({ holidays, groups, dict }: Props) {
                                                         id={`user-${option?.id}`}
                                                         name={`user-${option?.id}`}
                                                         value={option?.username}
-                                                        defaultChecked={filteredUser.includes(
-                                                            option
-                                                        )}
+                                                        checked={
+                                                            filteredUser.includes(option) &&
+                                                            !clearAll
+                                                        }
                                                         type="checkbox"
                                                         className="h-4 w-4 flex-shrink-0 rounded border-gray-300 text-primary focus:ring-primary"
                                                     />
